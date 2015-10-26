fd4d7df453e9a4ec381f18a69319cf6daefe155f
Jacob Moghtader <jake@moghtadeer.co>
2015-10-25 21:37:19 -0700

    locations working. menus have to be adapted.

--- app/models/point.rb
+++
@@ -1,6 +0,0 @@
-class Point < ActiveRecord::Base
-  geocoded_by :location
-  searchkick text_start: [:location], autocomplete: ['location']
-  after_validation :geocode
-
-end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :locations, dependent: :destroy
   belongs_to :user
 end
