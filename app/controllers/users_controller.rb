6042a479d0498b70c08f513bace5bb6fb89a0a81
Jeffrey Tai <jeffreytai@ucla.edu>
2015-11-05 21:24:51 -0800

    Added /follow page which combines /followers and /following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,29 @@ class UsersController < ApplicationController
     redirect_to root_url
   end
 
-  def following
-    @title = "Following"
-    @user = User.find_by(username: params[:username])
-    @users = @user.following
-    render 'show_follow'
+  def follow
+    if user_signed_in?
+      @user = User.find_by(username: params[:username])
+      @followings = @user.following
+      @followers = @user.followers
+      render 'show_follow'
+    else
+      redirect_to new_user_session_path
+    end
   end
 
-  def followers
-    @title = "Followers"
-    @user = User.find_by(username: params[:username])
-    @users = @user.followers
-    render 'show_follow'
-  end
+  # def following
+  #   @title = "Following"
+  #   @user = User.find_by(username: params[:username])
+  #   @users = @user.following
+  #   render 'show_follow'
+  # end
+  #
+  # def followers
+  #   @title = "Followers"
+  #   @user = User.find_by(username: params[:username])
+  #   @users = @user.followers
+  #   render 'show_follow'
+  # end
 
 end
