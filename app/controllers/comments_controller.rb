73ea12db76432b793f6617af2e5da4053b48cd23
Guanya Yang <guanya.yang@gmail.com>
2015-11-08 00:38:15 -0800

    trying to add blank comments now trigger an HTML warning on the same page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-  # validates :content, length: { maximum: 1000 },
-  #                     presence: true
+
   def create
     @trip = Trip.find(params[:trip_id])
     @comment = @trip.comments.create!(comment_params)
